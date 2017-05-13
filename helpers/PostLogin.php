741f04f4d97e0dd22c851f7f76be86ed175d946b
Scott Phillips <scottphillips@Scott-and-Alyces-iMac.local>
2017-05-13 16:36:38 +1000

    Added in email and DOB login validation into PostLogin->checkUser

--- helpers/PostLogin.php
+++ helpers/PostLogin.php
@@ -17,6 +17,25 @@
 
       public function checkUser() {
          session_start();
+         $emailRegexp = "/^[a-zA-Z0-9_.-]+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/";
+         // Validate Email address
+         if (preg_match($emailRegexp, $this->email) == false) {
+             $_SESSION['error'] = "Invalid email address.";
+             header('Location: /');
+             return;
+         }
+         $dobRegExp = '#^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|
+         (?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|
+         (?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^
+         (?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|
+         (?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|
+         (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$#';
+         // Validate Date of Birth
+         if (preg_match($dobRegExp, $this->dob) == false) {
+             $_SESSION['error'] = "Invalid date of birth.";
+             header('Location: /');
+             return;
+         }
          // Hash the email and dob together to create a password.
          $magic = hash("sha256", $this->email.$this->dob);
          // Create a new object of the login class
